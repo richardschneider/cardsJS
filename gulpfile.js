8bd66bc7a330737f5a90c6feca5a26f15513b6af
Richard Schneider <makaretu@gmail.com>
2015-04-05 13:33:18 +1200

    #15 gulp file to test and release

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var gulp   = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tag_version = require('gulp-tag-version');
+
+var paths = {
+  source: ['cards.js, cards-ko.js'],
+  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
+};
+
+var plumberConf = {};
+
+if (process.env.CI) {
+  plumberConf.errorHandler = function(err) {
+    throw err;
+  };
+}
+
+gulp.task('lint', function () {
+  return gulp.src(paths.source)
+    .pipe(plugins.jshint('.jshintrc'))
+    .pipe(plugins.plumber(plumberConf))
+    .pipe(plugins.jscs())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('istanbul', function (cb) {
+  gulp.src(paths.source)
+    .pipe(plugins.istanbul()) // Covering files
+    .pipe(plugins.istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(paths.tests)
+        .pipe(plugins.plumber(plumberConf))
+        .pipe(plugins.mocha())
+        .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
+        .on('finish', function() {
+          process.chdir(__dirname);
+          cb();
+        });
+    });
+});
+
+gulp.task('bump', ['test'], function () {
+  var bumpType = plugins.util.env.type || 'patch'; // major.minor.patch
+
+  return gulp.src(['./package.json', './bower.json'])
+    .pipe(plugins.bump({ type: bumpType }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('release', ['bump'], function () {
+    return gulp.src(['./package.json', './bower.json'])
+        .pipe(plugins.git.add())
+        .pipe(plugins.git.commit('rew release'))
+        .pipe(plugins.filter('package.json'))
+        .pipe(tag_version());
+});
+
+gulp.task('watch', ['test'], function () {
+  gulp.watch(paths.watch, ['test']);
+});
+
+gulp.task('test', ['lint', 'istanbul']);
+
+gulp.task('default', ['test']);
